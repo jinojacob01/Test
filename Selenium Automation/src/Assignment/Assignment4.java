b8c8ae5810fa8e880a31c5f82e46f64cd955da69
jinojacob01 <jinojacob456@gmail.com>
2022-10-07 22:20:13 +0530

    s

---
+++ Selenium Automation/src/Assignment/Assignment4.java
@@ -0,0 +1,33 @@
+package Assignment;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\jino.jacob\\Desktop\\eclipse-java-2022-09-R-win32-x86_64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://the-internet.herokuapp.com/");
+		driver.findElement(By.xpath("//a[text()='Multiple Windows']")).click();
+		driver.findElement(By.xpath("//div[@id='content']/div/a")).click();
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		String parentId = it.next();
+		String childId = it.next();
+		driver.switchTo().window(childId);
+		System.out.println(driver.findElement(By.cssSelector("div.example")).getText());
+		driver.switchTo().window(parentId);
+		System.out.println(driver.findElement(By.xpath("//div[@id='content']/div/h3")).getText());
+	}
+
+}
