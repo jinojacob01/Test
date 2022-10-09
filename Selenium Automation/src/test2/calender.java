c57e376ac0c27049bbba43d09b03a24189866b18
jinojacob01 <jinojacob456@gmail.com>
2022-10-09 21:03:17 +0530

    Oct9

--- Selenium Automation/src/test2/calender.java
+++ Selenium Automation/src/test2/calender.java
@@ -3,6 +3,7 @@ package test2;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,15 +22,20 @@ public class calender {
 		driver.manage().window().maximize();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.get("https://www.path2usa.com/travel-companion/");
-		System.out.println(driver.findElements(By.tagName("frame")).size());
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scroll(0,1200)");
+		
+		Thread.sleep(1000L);
 		driver.findElement(By.id("form-field-travel_from")).sendKeys("test");
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("form-field-travel_comp_date")));
-		Actions actions = new Actions(driver);
-		WebElement lang =   driver.findElement(By.id("form-field-travel_comp_language"));
-		Select language = new Select(lang);
-		WebElement submit = driver.findElement(By.xpath("//span[@class='elementor-button-text']"));
+		
+		
+//		Actions actions = new Actions(driver);
+//		WebElement lang =   driver.findElement(By.id("form-field-travel_comp_language"));
+//		Select language = new Select(lang);
 //		actions.scrollToElement(submit).build().perform();
-		language.selectByVisibleText("Hindi");
+//		language.selectByVisibleText("Hindi");
 		
 //		WebElement element = driver.findElement(By.id("form-field-travel_comp_date"));
 //		
