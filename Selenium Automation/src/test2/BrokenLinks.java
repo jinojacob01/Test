85af6f276cf08be9b0e1974c0b6a4786e7e8dbfc
jinojacob01 <jinojacob456@gmail.com>
2022-10-12 16:06:06 +0530

    Oct12

--- Selenium Automation/src/test2/BrokenLinks.java
+++ Selenium Automation/src/test2/BrokenLinks.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 public class BrokenLinks {
 
@@ -19,21 +20,26 @@ public class BrokenLinks {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\jino.jacob\\Desktop\\eclipse-java-2022-09-R-win32-x86_64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		SoftAssert sa = new SoftAssert();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/#");
 		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
-		for(WebElement link : links) {
-				String url = link.getAttribute("href");
-				HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
-				conn.setRequestMethod("HEAD");
-				conn.connect();
-				int responseCode = conn.getResponseCode();
-				System.out.println(responseCode);
-				if(responseCode>400) {
-					
-					System.out.println("Failed");
-					Assert.assertTrue(false);
-				}
+		for (WebElement link : links) {
+			String url = link.getAttribute("href");
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			int responseCode = conn.getResponseCode();
+			System.out.println(responseCode);
+			sa.assertTrue(responseCode < 400,
+					"The link with text " + link.getText() + " is broken with code " + responseCode);
+
+//				if(responseCode>400) {
+//					
+//					System.out.println("Failed");
+//					Assert.assertTrue(false);
+//				}
 		}
+		sa.assertAll();
 
 	}
 
