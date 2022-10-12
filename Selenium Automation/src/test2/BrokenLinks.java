933d03f185aa39cd001ab1f71df68f1cd0fd0ff5
jinojacob01 <jinojacob456@gmail.com>
2022-10-12 15:50:36 +0530

    Oct12

---
+++ Selenium Automation/src/test2/BrokenLinks.java
@@ -0,0 +1,40 @@
+package test2;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class BrokenLinks {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\jino.jacob\\Desktop\\eclipse-java-2022-09-R-win32-x86_64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/#");
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+		for(WebElement link : links) {
+				String url = link.getAttribute("href");
+				HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+				conn.setRequestMethod("HEAD");
+				conn.connect();
+				int responseCode = conn.getResponseCode();
+				System.out.println(responseCode);
+				if(responseCode>400) {
+					
+					System.out.println("Failed");
+					Assert.assertTrue(false);
+				}
+		}
+
+	}
+
+}
